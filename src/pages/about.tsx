0d06026f1b026d15242543f7b652a262c104a468
Jonas Rafael Duarte dos Santos <jonasrdsantos@gmail.com>
2023-05-15 12:30:01 -0300

    url

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,14 @@
 const About = () => {
-    return ( <div>oi</div> );
+    return (
+        <>
+            <h1>Sobre o projeto</h1>
+            <p>Esse projeto foi feito para fins acadêmicos, como atividade avaliativa.</p>
+            <p>A ideia é criar um jogo de cartas, onde cada carta é um gato, e cada gato tem atributos diferentes.</p>
+            <p>O baralho foi baseado no jogo "Super Trunfo"</p>
+            <p>Criador: Jonas Rafael Duarte dos Santos</p>
+            <p>GitHub: <a href="https://github.com/jonas-duarte/super-miaufo" target="_blank">https://github.com/jonas-duarte/super-miaufos</a></p>
+        </>
+    );
 }
- 
+
 export default About;
